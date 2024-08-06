b5e973b86028f1c099aa3ae85c0831b943759049
Taimur Ibrahim <tai.him18@gmail.com>
2024-08-06 22:30:50 +0200

    Update constants.py

--- autoscaler/constants.py
+++ autoscaler/constants.py
@@ -4,6 +4,6 @@ PROMETHEUS_ENDPOINT: Final[str] = 'http://192.168.49.2:32624'
 AVERAGE_PREDICTION_LATENCY_QUERY: Final[
     str] = 'sum(avg_over_time(predict_latency_sum[1m])) / sum(avg_over_time(predict_latency_count[1m]))'
 SCALING_INTERVAL: Final[int] = 15
-DESIRED_PREDICTION_LATENCY: Final[float] = 0.25  # seconds
+DESIRED_PREDICTION_LATENCY: Final[float] = 0.25  # time in seconds
 MIN_REPLICAS: Final[int] = 1
 MAX_REPLICAS: Final[int] = 10
